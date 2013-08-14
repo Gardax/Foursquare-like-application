04b1c7823a0ed1ac3eea27ad4e6f43fa01778261
PetarPenev <penev_90@abv.bg>
2013-08-14 13:15:24 +0300

    -created login services

---
+++ FoursquareApp/WebClient/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebClient.Models;
+using WebClient.Repositories;
+
+namespace WebClient.Controllers
+{
+    public class UserController : BaseApiController
+    {
+        [HttpPost]
+        [ActionName("register")]
+        public HttpResponseMessage RegisterUser(UserModel user)
+        {
+            var responseMsg = this.PerformOperation(() =>
+            {
+                UsersRepository.CreateUser(user.Username, user.AuthCode);
+                string username = string.Empty;
+                var sessionKey = UsersRepository.LoginUser(user.Username, user.AuthCode, out username);
+                return new UserLoggedModel()
+                {
+
+                    SessionKey = sessionKey
+                };
+            });
+            return responseMsg;
+        }
+
+        [HttpPost]
+        [ActionName("login")]
+        public HttpResponseMessage LoginUser(UserModel user)
+        {
+            var responseMsg = this.PerformOperation(() =>
+            {
+                string username = string.Empty;
+                var sessionKey = UsersRepository.LoginUser(user.Username, user.AuthCode, out username);
+                return new UserLoggedModel()
+                {
+                    SessionKey = sessionKey
+                };
+            });
+            return responseMsg;
+        }
+
+        [HttpGet]
+        [ActionName("logout")]
+        public HttpResponseMessage LogoutUser(string sessionKey)
+        {
+            var responseMsg = this.PerformOperation(() =>
+            {
+                UsersRepository.LogoutUser(sessionKey);
+            });
+            return responseMsg;
+        }
+    }
+}
